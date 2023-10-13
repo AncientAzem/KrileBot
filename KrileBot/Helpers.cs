6f41d682d5a22770f4b43ed7fe8cefbdc55e7446
Colin (Cielo/Khal) <azem@baldesion.dev>
2023-10-12 22:29:05 -0500

    Added Proper Solution Structure

---
+++ KrileBot/Helpers.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace KrileDotNet;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Helpers
+{
+    public static string FormatStringWithSpaces(object s)
+    {
+        return string.Concat((s.ToString() ?? string.Empty).Select(x => Char.IsUpper(x) ? " " + x : x.ToString())).TrimStart(' ');
+    }
+
+    public static Dictionary<string, List<string>> FFXIV_Zones => new Dictionary<string, List<string>>()
+    {
+        {"Endwalker", Zones_EW},
+        {"Shadowbringers", Zones_ShB},
+        {"Stormblood", Zones_SB},
+        {"Heavensward", Zones_HW},
+        {"A Realm Reborn", Zones_ARR},
+    };
+
+    private static List<string> Zones_EW => new List<string>()
+    {
+        "Radz-at-Han",
+        "Thavnair",
+        "Garlemald",
+        "Old Sharlayan", 
+        "Labyrinthos", 
+        "Mare Lamentorum", 
+        "Ultima Thule", 
+        "Elpis"
+    };
+
+    private static List<string> Zones_ShB => new List<string>()
+    {
+        "Lakeland",
+        "Kholusia",
+        "Amh Araeng",
+        "Il Mheg",
+        "The Rak'tika Greatwood",
+        "The Tempest"
+    };
+    
+    private static List<string> Zones_SB => new List<string>()
+    {
+        "The Ruby Sea",
+        "Yanxia",
+        "The Azim Step",
+        "The Fringes",
+        "The Peaks",
+        "The Lochs"
+    };
+    
+    private static List<string> Zones_HW => new List<string>()
+    {
+        "Coerthas Western Highlands",
+        "The Sea of Clouds",
+        "Azys Lla",
+        "The Dravanian Forelands",
+        "The Churning Mists"
+    };
+    
+    private static List<string> Zones_ARR => new List<string>()
+    {
+        "Western Thanalan",
+        "Central Thanalan",
+        "Eastern Thanalan",
+        "Southern Thanalan",
+        "Northern Thanalan",
+        "The Gold Saucer",
+        "Coerthas Central Highlands",
+        "Mor Dhona",
+        "Central Shroud",
+        "East Shroud",
+        "South Shroud",
+        "North Shroud",
+        "Middle La Noscea",
+        "Lower La Noscea",
+        "Eastern La Noscea",
+        "Western La Noscea",
+        "Upper La Noscea",
+        "Outer La Noscea"
+    };
+}
---
+++ KrileBot/Services/DiscordClientService.cs
@@ -0,0 +1,116 @@
+﻿using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.Net;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace KrileDotNet.Services;
+
+public class DiscordClientService : IHostedService
+{
+    private static DiscordSocketClient? _client;
+    private static InteractionService? _interactionService;
+    private static CommandService? _commandService;
+    private static IServiceProvider? _serviceProvider;
+    private static HuntRelayService _huntRelayService;
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        if (_client is null)
+        {
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.All
+            };
+            
+            _client = new DiscordSocketClient(config);
+            _client.Log += LogDiscordMessage;
+            _client.Ready += ClientReady;
+        }
+
+        if (_interactionService is null)
+        {
+            _interactionService = new InteractionService(_client.Rest);
+        }
+
+        if (_commandService is null)
+        {
+            _commandService = new CommandService();
+        }
+        
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton(_client)
+            .AddSingleton(_commandService)
+            .AddSingleton(_interactionService)
+            .BuildServiceProvider();
+        
+        // Hunt Provider
+        // HuntRelayFormatter.Setup(827202823774666792, 1071183795585290290, 1016065075875938334); // Actual Server
+        
+
+        var botToken = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+        await _client.LoginAsync(TokenType.Bot, botToken);
+        await _client.StartAsync();
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _client!.StopAsync();
+        await _client.LogoutAsync();
+    }
+
+    private static async Task ClientReady()
+    {
+        try
+        {
+            await _client!.SetActivityAsync(new Game("Ejika's theories", ActivityType.Listening));
+            await _interactionService!.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+            var guildId = Environment.GetEnvironmentVariable("GUILD_ID");
+            if (guildId is not null)
+            {
+                await LogDiscordMessage(new LogMessage(LogSeverity.Info, "Startup", "Initializing Guild Commands"));
+                await _interactionService.RegisterCommandsToGuildAsync(Convert.ToUInt64(guildId));
+            }
+            else
+            {
+                await LogDiscordMessage(new LogMessage(LogSeverity.Info, "Startup", "Initializing Global Commands"));
+                await _interactionService.RegisterCommandsGloballyAsync();
+            }
+            
+            // Setup Hunts
+            _huntRelayService = new HuntRelayService(ref _client,
+                ulong.Parse(Environment.GetEnvironmentVariable("RELAY_SERVER_ID")!), 
+                ulong.Parse(Environment.GetEnvironmentVariable("RELAY_CHANNEL_ID")!), 
+                ulong.Parse(Environment.GetEnvironmentVariable("SONAR_CHANNEL_ID")!));
+        }
+        catch (HttpException exception)
+        {
+            var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+
+        _client!.InteractionCreated += async interaction =>
+        {
+            var ctx = new SocketInteractionContext(_client, interaction);
+            await _interactionService!.ExecuteCommandAsync(ctx, _serviceProvider);
+        };
+        
+        _client!.MessageReceived += async interaction =>
+        {
+            if (interaction.Channel.Id == ulong.Parse(Environment.GetEnvironmentVariable("SONAR_CHANNEL_ID")!))
+            {
+                await LogDiscordMessage(new LogMessage(LogSeverity.Debug, "MessageHandler", $"New Sonar Relay [Raw Data]: {interaction.Content}"));
+                await _huntRelayService.ProcessSonarReport(interaction.Content);
+            }
+        };
+    }
+    private static Task LogDiscordMessage(LogMessage message)
+    {
+        Console.WriteLine(message);
+        return Task.CompletedTask;
+    }
+}
